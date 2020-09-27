f6d3616936f91ce45f3abedafcd86845e9acc669
Karol Kostyra <karol.m.kostyra@gmail.com>
2020-09-27 12:59:43 +0200

    add ship movement & camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] ShipController shipController;
+    [SerializeField] float smoothTime = 0f;
+    private Vector3 velocity = Vector3.zero;
+
+    private void LateUpdate()
+    {
+        if (shipController != null && !shipController.IsDestroyed())
+        {
+            gameObject.transform.position =
+                new Vector3(shipController.transform.position.x, shipController.transform.position.y, gameObject.transform.position.z);
+        }
+    }
+}

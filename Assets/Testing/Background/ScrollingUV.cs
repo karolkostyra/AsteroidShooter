1bd728dc4a166de713e3481fae36740b1f5cae85
Karol Kostyra <karol.m.kostyra@gmail.com>
2020-10-08 14:27:40 +0200

    Make movable background with depth effect

---
+++ Assets/Testing/Background/ScrollingUV.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingUV : MonoBehaviour
+{
+    [SerializeField] float parallax = 8f;
+
+    void LateUpdate()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        Material material = meshRenderer.material;
+        Vector2 offset = material.mainTextureOffset;
+
+        offset.x = -transform.position.x / transform.localScale.x / parallax;
+        offset.y = -transform.position.y / transform.localScale.y / parallax;
+
+        material.mainTextureOffset = offset;
+    }
+}

0bfea4c704e6373694d7ab70a4aa1bd01153910e
Karol Kostyra <karol.m.kostyra@gmail.com>
2020-10-14 20:00:55 +0200

    Implement application pause and resume

---
+++ Assets/Scripts/ApplicationPause.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ApplicationPause : MonoBehaviour
+{
+    private bool gameStarted = false;
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        if (hasFocus && gameStarted)
+        {
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void GameStarted(bool _started)
+    {
+        gameStarted = _started;
+    }
+}
